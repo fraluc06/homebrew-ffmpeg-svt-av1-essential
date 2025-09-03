9ca360317dbec2ba15731b4201a55d2f2744a336
Francesco Lucarelli <francesco06lucarelli@gmail.com>
2025-09-03 19:11:58 +0200

    Update ffmpeg-custom.rb: adjust revision, add livecheck, and improve test case

--- Formula/ffmpeg-custom.rb
+++ Formula/ffmpeg-custom.rb
@@ -3,9 +3,16 @@ class FfmpegCustom < Formula
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-8.0.tar.xz"
   sha256 "b2751fccb6cc4c77708113cd78b561059b6fa904b24162fa0be2d60273d27b8e"
+  # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
+  # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
   license "GPL-2.0-or-later"
-  revision 2
+  revision 1
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
+
+  livecheck do
+    url "https://ffmpeg.org/download.html"
+    regex(/href=.*?ffmpeg[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   depends_on "pkgconf" => :build
   depends_on "aom"
@@ -38,7 +45,7 @@ class FfmpegCustom < Formula
   depends_on "snappy"
   depends_on "speex"
   depends_on "srt"
-  depends_on "svt-av1-essential"  # 👈 sostituisce la svt-av1 standard
+  depends_on "svt-av1-essential"  # replaces the standard svt-av1
   depends_on "tesseract"
   depends_on "theora"
   depends_on "webp"
@@ -69,7 +76,15 @@ class FfmpegCustom < Formula
     depends_on "nasm" => :build
   end
 
+  # Fix for QtWebEngine, do not remove
+  # https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=270209
+  patch do
+    url "https://gitlab.archlinux.org/archlinux/packaging/packages/ffmpeg/-/raw/5670ccd86d3b816f49ebc18cab878125eca2f81f/add-av_stream_get_first_dts-for-chromium.patch"
+    sha256 "57e26caced5a1382cb639235f9555fc50e45e7bf8333f7c9ae3d49b3241d3f77"
+  end
+
   def install
+    # The new linker leads to duplicate symbol issue https://github.com/homebrew-ffmpeg/homebrew-ffmpeg/issues/140
     ENV.append "LDFLAGS", "-Wl,-ld_classic" if DevelopmentTools.clang_build_version >= 1500
 
     args = %W[
@@ -126,18 +141,23 @@ class FfmpegCustom < Formula
       --disable-indev=jack
     ]
 
+    # Needs corefoundation, coremedia, corevideo
     args += %w[--enable-videotoolbox --enable-audiotoolbox] if OS.mac?
     args << "--enable-neon" if Hardware::CPU.arm?
 
     system "./configure", *args
     system "make", "install"
 
+    # Build and install additional FFmpeg tools
     system "make", "alltools"
     bin.install (buildpath/"tools").children.select { |f| f.file? && f.executable? }
     pkgshare.install buildpath/"tools/python"
   end
 
   test do
-    system bin/"ffmpeg", "-filters"
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    assert_path_exists mp4out
   end
 end
