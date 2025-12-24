1935d7941b0c02df990b1128ccd3e00503d04035
Francesco Lucarelli <francesco06lucarelli@gmail.com>
2025-12-25 00:42:37 +0100

    Remove bottle block from SVT-AV1-Essential formula and update CI condition for pull requests

--- Formula/svt-av1-essential.rb
+++ Formula/svt-av1-essential.rb
@@ -10,14 +10,6 @@ class SvtAv1Essential < Formula
     regex(/^v?(\d+(?:\.\d+)+-Essential)$/i)
   end
 
-  bottle do
-    root_url "https://github.com/fraluc06/homebrew-ffmpeg-svt-av1-essential/releases/download/svt-av1-essential-v3.1.2"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "30e3cd619d33b498940d303dda254d55e0622adbe4374b1f08b6b1a34a7d37c5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c0ca090537428f084ec4ec348759104c52318014c9f0b76e8df7391c880b6e09"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5165a57ff121d5207493f3db0cb443b8b7f32d25f8b5515f0fef3c33fbb2688d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f45479f24595d0a8871265b8b9264a96eabf27ded2c439c074b8b7dba6ebdf0d"
-  end
-
   depends_on "cmake" => :build
   depends_on "nasm" => :build
 
