095974b625341c67084398e71ba3caf7f7d1fc13
Francesco Lucarelli <67560467+fraluc06@users.noreply.github.com>
2025-12-24 23:53:07 +0000

    svt-av1-essential: add 3.1.2 bottle.

--- Formula/svt-av1-essential.rb
+++ Formula/svt-av1-essential.rb
@@ -10,6 +10,14 @@ class SvtAv1Essential < Formula
     regex(/^v?(\d+(?:\.\d+)+-Essential)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/fraluc06/homebrew-ffmpeg-svt-av1-essential/releases/download/svt-av1-essential-3.1.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c6114e36a1089e6939cab79a4dd0169a7341d198a0a96cfa85f8ab88adce9bfa"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5ce854e792555f4baf5b261f4daa016736ccb2ff125b7950cfef4f8a26bd96f1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "86e5745b3e3125c4ea7552a8baf27b43f35a69ce62e2e1b7802ec765791becbe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7d2024e1c07c31b48d33c109d1680c58ed1c714cf28f92d6e495b47722241c75"
+  end
+
   depends_on "cmake" => :build
   depends_on "nasm" => :build
 
