b61ccb4000d25ddb88fe45a0d5206c31672e73de
Francesco Lucarelli <francesco06lucarelli@gmail.com>
2026-08-03 12:52:44 +0200

    fix(ffmpeg): disable xlib and libxcb on macOS to prevent indirect linkage

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -140,7 +140,7 @@ class Ffmpeg < Formula
     ]
 
     # Needs corefoundation, coremedia, corevideo
-    args += %w[--enable-videotoolbox --enable-audiotoolbox] if OS.mac?
+    args += %w[--enable-videotoolbox --enable-audiotoolbox --disable-xlib --disable-libxcb] if OS.mac?
     args << "--enable-neon" if Hardware::CPU.arm?
 
     system "./configure", *args
