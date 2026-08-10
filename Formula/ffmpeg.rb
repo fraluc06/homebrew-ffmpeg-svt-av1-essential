1698dc20c035bfb853c9237011f1d27a076469a9
Francesco Lucarelli <francesco06lucarelli@gmail.com>
2026-08-10 10:34:28 +0200

    fix(ffmpeg): update license, ABI version and remove stale bottles

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -3,17 +3,12 @@ class Ffmpeg < Formula
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-9.0.tar.xz"
   sha256 "7f607a00dd0d28a729d5a4811205812eef01cf6ef6155025febb6f36a9062d52"
-  license "GPL-2.0-or-later"
+  license "GPL-3.0-or-later"
+  compatibility_version 3
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
   livecheck do
     formula "ffmpeg"
-  end
-
-  bottle do
-    root_url "https://github.com/fraluc06/homebrew-ffmpeg-svt-av1-essential/releases/download/ffmpeg-8.1.2"
-    sha256 arm64_tahoe:  "5301644ddb1d18ae06c843735fe978e79a5773d79ea85b5363031f3809204245"
-    sha256 x86_64_linux: "78f6bf0430b9e87c60ccff090f8e51404e1099002c2a8a45e408364fd7b178de"
   end
 
   depends_on "pkgconf" => :build
